a5d93bbb1f23fd6785b90f83723a8c3560beb361
Sorath <santiagoacostaavila2905@gmail.com>
2025-05-19 22:48:17 -0500

    build

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -o errexit
 
-cd RecetaPag  # Entra a la carpeta que contiene manage.py
+cd alimentos  # Entra a la carpeta que contiene manage.py
 pip install -r ../requirements.txt
 
 python manage.py collectstatic --no-input
